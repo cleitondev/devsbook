f4c4b561d547e558576c27359a353d80235d9bec
Cleiton Edgard <cleiton.weeb@gmail.com>
2021-05-01 01:30:05 -0300

    Validaão de dados da pagina de login

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -2,14 +2,37 @@
 namespace src\controllers;
 
 use \core\Controller;
+use \src\handlers\LoginHandler;
 
 class LoginController extends Controller {
     public function signin(){
-        $this->render('login');
+        $flash = '';
+        if(!empty($_SESSION['flash'])){
+            $flash = $_SESSION['flash'];
+            $_SESSION['flash'] = '';
+        }
+        $this->render('login', [
+            'flash' => $flash
+        ]);
     }
 
     public function signinAction(){
-        echo "Login Recebido!";
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+        $passwor = filter_input(INPUT_POST, 'password');
+
+        if($passwor & $email){
+            $token = LoginHandler::verifyLogin($email, $passwor);
+            if($token){
+                $_SESSION['token'] = $token;
+                $this->redirect('/');
+            }else{
+                $_SESSION['flash'] = 'Email e/ou senha não confere';
+                $this->redirect('/login');
+            }
+        }else{
+            $_SESSION['flash'] = 'Digite os campos de email e/ou senha!';
+            $this->redirect('/login');
+        }
     }
 
     public function signup(){
--- src/handlers/LoginHandler.php
+++ src/handlers/LoginHandler.php
@@ -10,13 +10,29 @@ class LoginHandler {
             $data = User::select()->where('token', $token)->one();
             if(count($data) > 0){
                 $loggedUser = new User();
-                $loggedUser->setId($data['id']);
-                $loggedUser->setEmail($data['email']);
-                $loggedUser->setName($data['name']);
+                $loggedUser->id = $data['id'];
+                $loggedUser->email = $data['email'];
+                $loggedUser->name = $data['name'];
 
                 return $loggedUser;
             } 
         } 
         return false;
     }
+    public static function verifyLogin($email, $passwor){
+        $user = User::select()->where('email', $email)->one();
+
+        if($user) {
+            if(password_verify($passwor, $user['password'])){
+                $token = md5(time(), rand(0,9999),time());
+
+                User::update()->set('token', $token)->where('email', $email);
+
+                return $token;
+            }
+        } else {
+            return false;
+        }
+    }
 }
+
